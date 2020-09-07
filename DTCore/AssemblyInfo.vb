10a30c70b640e942d55e7f6202b6aecd3fd66057
Nathan Moschkin <iroywrit@gmail.com>
2020-09-06 21:57:51 -0400

    Initial Commit

---
+++ DTCore/AssemblyInfo.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+Imports System.Runtime.CompilerServices
+
+
+<Assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)>
+
+<Assembly: InternalsVisibleTo("DTInterop", AllInternalsVisible:=True)>
+<Assembly: InternalsVisibleTo("DTMath", AllInternalsVisible:=True)>
+
